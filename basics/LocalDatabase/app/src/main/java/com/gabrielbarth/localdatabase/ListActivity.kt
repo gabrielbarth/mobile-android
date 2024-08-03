36b831d23bbc89af249abd5a6dd0ea57897224e3
gabrielbarth <gabriel.silverio@stone.com.br>
2024-08-03 12:58:40 -0400

    feat: add binding on ListActivity

--- basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/ListActivity.kt
+++ basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/ListActivity.kt
@@ -2,10 +2,21 @@ package com.gabrielbarth.localdatabase
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import com.gabrielbarth.localdatabase.databinding.ActivityListBinding
 
 class ListActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
+        // setContentView(R.layout.activity_list)
+
+        binding = ActivityListBinding.inflate( layoutInflater )
+        setContentView(binding.root)
+
+        val registros = listOf<String>( "Brasil", "Argentina", "Paraguai", "Uruguai" )
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, registros )
+        binding.lvPrincipal.adapter = adapter
     }
 }
