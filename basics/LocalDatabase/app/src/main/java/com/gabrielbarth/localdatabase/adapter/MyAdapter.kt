10e9393f32f0f4152fcbc6e109768a6dee4b4c03
gabrielbarth <gabriel.silverio@stone.com.br>
2024-08-10 10:15:35 -0400

    feat: add adapter package and class

---
+++ basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/adapter/MyAdapter.kt
@@ -0,0 +1,23 @@
+package com.gabrielbarth.localdatabase.adapter;
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class MyAdapter : BaseAdapter() {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Any {
+
+    }
+
+    override fun getItemId(position: Int): Long {
+
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+    }
+}
