26e5b2a065eedc880e25d7f409711025a8a1c63d
gabrielbarth <gabriel.silverio@stone.com.br>
2024-08-10 11:06:28 -0400

    feat: add edit button and navigate action to MainActivity

--- basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/adapter/MyAdapter.kt
+++ basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/adapter/MyAdapter.kt
@@ -1,14 +1,17 @@
 package com.gabrielbarth.localdatabase.adapter;
 
 import android.content.Context
+import android.content.Intent
 import android.database.Cursor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageButton
 import android.widget.TextView
 import com.gabrielbarth.localdatabase.R
 import com.gabrielbarth.localdatabase.entity.Cadastro
+import com.gabrielbarth.localdatabase.MainActivity
 
 class MyAdapter(val context: Context, val cursor: Cursor) : BaseAdapter() {
     override fun getCount(): Int {
@@ -38,13 +41,28 @@ class MyAdapter(val context: Context, val cursor: Cursor) : BaseAdapter() {
 
         val tvNomeElementoLista = v.findViewById<TextView>(R.id.tvNomeElementoLista)
         val tvTelefoneElementoLista = v.findViewById<TextView>(R.id.tvTelefoneElementoLista)
+        val btEditarElementoLista = v.findViewById<ImageButton>( R.id.btEditarElementoLista )
 
         cursor.moveToPosition(position)
 
         tvNomeElementoLista.setText(cursor.getString(1))
         tvTelefoneElementoLista.setText(cursor.getString(2))
 
+        btEditarElementoLista.setOnClickListener{
+            cursor.moveToPosition( position )
+
+            val intent = Intent( context, MainActivity::class.java)
+
+            intent.putExtra( "cod", cursor.getInt( 0 ) )
+            intent.putExtra( "nome", cursor.getString( 1 ) )
+            intent.putExtra( "telefone", cursor.getString( 2 ) )
+
+            context.startActivity( intent )
+        }
+
         return v
     }
+
+
 
 }
