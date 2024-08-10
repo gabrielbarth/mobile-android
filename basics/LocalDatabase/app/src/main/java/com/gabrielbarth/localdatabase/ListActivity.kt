eda358a1f22ec372bb5c0c5f31f93c95c8f9fce4
gabrielbarth <gabriel.silverio@stone.com.br>
2024-08-10 10:26:35 -0400

    feat: set get functions on adapter and implement adapter on ListActivity

--- basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/ListActivity.kt
+++ basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/ListActivity.kt
@@ -1,10 +1,9 @@
 package com.gabrielbarth.localdatabase
 
-import android.R
 import android.database.Cursor
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.SimpleCursorAdapter
+import com.gabrielbarth.localdatabase.adapter.MyAdapter
 import com.gabrielbarth.localdatabase.databinding.ActivityListBinding
 import com.gabrielbarth.localdatabase.database.DatabaseHandler
 
@@ -21,14 +20,7 @@ class ListActivity : AppCompatActivity() {
 
         val registros : Cursor = banco.cursorList() //fonte  tem que ter selecionado o campo _id, com exatamente este nome
 
-        val adapter = SimpleCursorAdapter(
-            this,
-            R.layout.simple_list_item_2,
-            registros,
-            arrayOf( "nome", "telefone" ),
-            intArrayOf( R.id.text1, R.id.text2 ),
-            0
-        ) //meio de campo
+        val adapter = MyAdapter( this, registros )
 
         binding.lvPrincipal.adapter = adapter //destino
     }
--- basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/adapter/MyAdapter.kt
+++ basics/LocalDatabase/app/src/main/java/com/gabrielbarth/localdatabase/adapter/MyAdapter.kt
@@ -1,23 +1,50 @@
 package com.gabrielbarth.localdatabase.adapter;
 
+import android.content.Context
+import android.database.Cursor
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
+import com.gabrielbarth.localdatabase.R
+import com.gabrielbarth.localdatabase.entity.Cadastro
 
-class MyAdapter : BaseAdapter() {
+class MyAdapter(val context: Context, val cursor: Cursor) : BaseAdapter() {
     override fun getCount(): Int {
-
+        return cursor.count
     }
 
     override fun getItem(position: Int): Any {
-
+        cursor.moveToPosition(position)
+        val cadastro = Cadastro(
+            cursor.getInt(0),
+            cursor.getString(1),
+            cursor.getString(2)
+        )
+        return cadastro
     }
 
     override fun getItemId(position: Int): Long {
 
+        cursor.moveToPosition(position)
+        return cursor.getLong(0)
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val v = inflater.inflate(R.layout.list_element, null)
 
+        val tvNomeElementoLista = v.findViewById<TextView>(R.id.tvNomeElementoLista)
+        val tvTelefoneElementoLista = v.findViewById<TextView>(R.id.tvTelefoneElementoLista)
+
+        cursor.moveToPosition(position)
+
+        tvNomeElementoLista.setText(cursor.getString(1))
+        tvTelefoneElementoLista.setText(cursor.getString(2))
+
+        return v
     }
+
 }
