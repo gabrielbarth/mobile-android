63400c98eadd9b1275c141c0fd9cc9780e5d37f1
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-02 22:45:56 -0400

    feat: add date picker component sample

--- basics/VisualComponents/app/src/main/java/com/gabrielbarth/visualcomponents/MainActivity.kt
+++ basics/VisualComponents/app/src/main/java/com/gabrielbarth/visualcomponents/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.gabrielbarth.visualcomponents
 
+import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
+import android.widget.DatePicker
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -16,11 +18,13 @@ class MainActivity : AppCompatActivity() {
 //    private lateinit var rgSexo : RadioGroup
 //    private lateinit var rbMasculino : RadioButton
 //    private lateinit var rbFeminino : RadioButton
+//
+//    private lateinit var cbMasculino : CheckBox
+//    private lateinit var cbFeminino : CheckBox
 
-    private lateinit var cbMasculino : CheckBox
-    private lateinit var cbFeminino : CheckBox
+    private lateinit var dpNasc: DatePicker
 
-    private lateinit var main : LinearLayout
+    private lateinit var main: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,27 +34,43 @@ class MainActivity : AppCompatActivity() {
 //        rbMasculino = findViewById(R.id.rbMasculino)
 //        rbFeminino = findViewById(R.id.rbFeminino)
 
-        cbMasculino = findViewById(R.id.cbMasculino)
-        cbFeminino = findViewById(R.id.cbFeminino)
+//        cbMasculino = findViewById(R.id.cbMasculino)
+//        cbFeminino = findViewById(R.id.cbFeminino)
+
+        dpNasc = findViewById(R.id.dpNasc)
 
         main = findViewById(R.id.main)
 
-
-        cbFeminino.setOnCheckedChangeListener {_, checkedId ->
-           Snackbar.make(main, "Feminino selecionado", Snackbar.LENGTH_SHORT).show()
-        }
+//        cbFeminino.setOnCheckedChangeListener {_, checkedId ->
+//           Snackbar.make(main, "Feminino selecionado", Snackbar.LENGTH_SHORT).show()
+//        }
     }
 
     fun btTestComponentOnClick(view: View) {
-        if( cbMasculino.isChecked ){
-            val snackbar: Snackbar = Snackbar.make(main, "Masculino selecionado", Snackbar.LENGTH_SHORT)
+//        if( cbMasculino.isChecked ){
+//            val snackbar: Snackbar = Snackbar.make(main, "Masculino selecionado", Snackbar.LENGTH_SHORT)
+//
+//            snackbar.setBackgroundTint(Color.GRAY)
+//            snackbar.setAction("Cancelar") {
+//                snackbar.dismiss()
+//            }
+//
+//            snackbar.show()
+//        }
 
-            snackbar.setBackgroundTint(Color.GRAY)
-            snackbar.setAction("Cancelar") {
-                snackbar.dismiss()
-            }
+        // dpNasc em tela
+//        val dateFormatted = "${dpNasc.dayOfMonth}/${dpNasc.month + 1}/${dpNasc.year}"
+//        Snackbar.make(main, dateFormatted, Snackbar.LENGTH_SHORT).show()
 
-            snackbar.show()
-        }
+        // dpNasc em modal
+        val dateDialog = DatePickerDialog(
+            this,
+            {_, selectedYear, selectedMonth, selectedDay ->
+                val dateFormatted = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+                Snackbar.make(main, dateFormatted, Snackbar.LENGTH_SHORT).show()
+            },
+            2024, 7, 25
+        )
+        dateDialog.show()
     }
 }
