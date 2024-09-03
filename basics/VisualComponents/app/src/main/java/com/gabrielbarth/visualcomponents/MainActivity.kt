9d55056fe779e448cd4248452237f19dc3328b11
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-02 22:34:30 -0400

    feat: add checkbox component sample

--- basics/VisualComponents/app/src/main/java/com/gabrielbarth/visualcomponents/MainActivity.kt
+++ basics/VisualComponents/app/src/main/java/com/gabrielbarth/visualcomponents/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gabrielbarth.visualcomponents
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -12,9 +13,12 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var rgSexo : RadioGroup
-    private lateinit var rbMasculino : RadioButton
-    private lateinit var rbFeminino : RadioButton
+//    private lateinit var rgSexo : RadioGroup
+//    private lateinit var rbMasculino : RadioButton
+//    private lateinit var rbFeminino : RadioButton
+
+    private lateinit var cbMasculino : CheckBox
+    private lateinit var cbFeminino : CheckBox
 
     private lateinit var main : LinearLayout
 
@@ -22,20 +26,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rgSexo = findViewById(R.id.rgSexo)
-        rbMasculino = findViewById(R.id.rbMasculino)
-        rbFeminino = findViewById(R.id.rbFeminino)
+//        rgSexo = findViewById(R.id.rgSexo)
+//        rbMasculino = findViewById(R.id.rbMasculino)
+//        rbFeminino = findViewById(R.id.rbFeminino)
+
+        cbMasculino = findViewById(R.id.cbMasculino)
+        cbFeminino = findViewById(R.id.cbFeminino)
 
         main = findViewById(R.id.main)
 
 
-        rgSexo.setOnCheckedChangeListener {_, checkedId ->
-           Snackbar.make(main, "Componente selecionado", Snackbar.LENGTH_SHORT).show()
+        cbFeminino.setOnCheckedChangeListener {_, checkedId ->
+           Snackbar.make(main, "Feminino selecionado", Snackbar.LENGTH_SHORT).show()
         }
     }
 
     fun btTestComponentOnClick(view: View) {
-        if( rbMasculino.isChecked ){
+        if( cbMasculino.isChecked ){
             val snackbar: Snackbar = Snackbar.make(main, "Masculino selecionado", Snackbar.LENGTH_SHORT)
 
             snackbar.setBackgroundTint(Color.GRAY)
