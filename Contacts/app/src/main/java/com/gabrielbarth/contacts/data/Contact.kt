fd4f61d05fe4b50702c6b8f0285391163c0c169c
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-28 08:42:55 -0400

    feat: add contact model data class

--- Contacts/app/src/main/java/com/gabrielbarth/contacts/data/Contact.kt
+++ Contacts/app/src/main/java/com/gabrielbarth/contacts/data/Contact.kt
@@ -1,4 +1,10 @@
 package com.gabrielbarth.contacts.data
 
-class Contact {
-}
+data class Contact(
+    val id: Int = 0,
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
+    val isFavorite: Boolean = false
+)
