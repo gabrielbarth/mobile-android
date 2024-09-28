e4b22ea83cf74f9c1b969373484fe8b1fd00d114
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-28 12:45:50 -0400

    feat: add extention formatter functions

---
+++ Contacts/app/src/main/java/com/gabrielbarth/contacts/ui/utils/Extensions.kt
@@ -0,0 +1,27 @@
+package com.gabrielbarth.contacts.ui.utils
+
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+fun String.toFormattedPhone(): String = mapIndexed { index, char ->
+    when {
+        index == 0 -> "($char"
+        index == 2 -> ") $char"
+        (index == 6 && length < 11) ||
+                (index == 7 && length == 11) -> "-$char"
+
+        else -> char
+    }
+}.joinToString("")
+
+fun BigDecimal.format(): String {
+    val formatter = DecimalFormat("R$#,##0.00")
+    return formatter.format(this)
+}
+
+fun LocalDate.format(): String {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    return format(formatter)
+}
