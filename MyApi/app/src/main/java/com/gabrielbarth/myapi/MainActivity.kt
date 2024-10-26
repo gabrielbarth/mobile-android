d3cfd41b8ee3d08bfab77317ddf7d8a7d9ef3f01
gabrielbarth <gabriel.silverio@stone.com.br>
2024-10-26 01:30:17 -0400

    feat: add xml list item layout

--- MyApi/app/src/main/java/com/gabrielbarth/myapi/MainActivity.kt
+++ MyApi/app/src/main/java/com/gabrielbarth/myapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gabrielbarth.myapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gabrielbarth.myapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
