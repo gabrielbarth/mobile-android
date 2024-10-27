5fe6547ef821a96eb19e7d72084088720be7e06b
gabrielbarth <gabriel.silverio@stone.com.br>
2024-10-26 22:08:19 -0400

    post new item

--- MyApi/app/src/main/java/com/gabrielbarth/myapi/NewItemActivity.kt
+++ MyApi/app/src/main/java/com/gabrielbarth/myapi/NewItemActivity.kt
@@ -3,8 +3,19 @@ package com.gabrielbarth.myapi
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gabrielbarth.myapi.databinding.ActivityNewItemBinding
+import com.gabrielbarth.myapi.model.ItemValue
+import com.gabrielbarth.myapi.service.Result
+import com.gabrielbarth.myapi.service.RetrofitClient
+import com.gabrielbarth.myapi.service.safeApiCall
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.security.SecureRandom
+import java.util.Date
 
 class NewItemActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewItemBinding
@@ -22,6 +33,87 @@ class NewItemActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
+        binding.saveCta.setOnClickListener {
+            save()
+        }
+    }
+
+    private fun save() {
+        if (!validateForm()) return
+        CoroutineScope(Dispatchers.IO).launch {
+            val id = SecureRandom().nextInt().toString()
+            val itemValue = ItemValue(
+                id,
+                binding.name.text.toString(),
+                binding.surname.text.toString(),
+                binding.address.text.toString(),
+                binding.imageUrl.text.toString(),
+                binding.age.text.toString().toInt(),
+                Date()
+            )
+            val result = safeApiCall { RetrofitClient.apiService.addItem(itemValue) }
+            withContext(Dispatchers.Main) {
+                when (result) {
+                    is Result.Error -> {
+                        Toast.makeText(
+                            this@NewItemActivity,
+                            R.string.error_create,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    is Result.Success -> {
+                        Toast.makeText(
+                            this@NewItemActivity,
+                            getString(R.string.success_create, result.data.id),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun validateForm(): Boolean {
+        if (binding.name.text.toString().isBlank()) {
+            Toast.makeText(
+                this,
+                getString(R.string.error_validate_form, "Name"),
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        if (binding.surname.text.toString().isBlank()) {
+            Toast.makeText(
+                this,
+                getString(R.string.error_validate_form, "Surname"),
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        if (binding.age.text.toString().isBlank()) {
+            Toast.makeText(this, getString(R.string.error_validate_form, "Age"), Toast.LENGTH_SHORT)
+                .show()
+            return false
+        }
+        if (binding.imageUrl.text.toString().isBlank()) {
+            Toast.makeText(
+                this,
+                getString(R.string.error_validate_form, "Image Url"),
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        if (binding.address.text.toString().isBlank()) {
+            Toast.makeText(
+                this,
+                getString(R.string.error_validate_form, "Address"),
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        return true
     }
 
     companion object {
--- MyApi/app/src/main/java/com/gabrielbarth/myapi/service/ApiService.kt
+++ MyApi/app/src/main/java/com/gabrielbarth/myapi/service/ApiService.kt
@@ -1,8 +1,11 @@
 package com.gabrielbarth.myapi.service
 
 import com.gabrielbarth.myapi.model.Item
+import com.gabrielbarth.myapi.model.ItemValue
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ApiService {
@@ -14,4 +17,7 @@ interface ApiService {
 
     @DELETE("items/{id}")
     suspend fun deleteItem(@Path("id") id: String)
+
+    @POST("items")
+    suspend fun addItem(@Body item: ItemValue): Item
 }
